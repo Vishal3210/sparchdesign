3208f870997675f4b951274a7111ae15e61302eb
Vishal Thakur <vishalsanjivthakur@gmail.com>
2023-04-16 23:19:29 +0530

    Functional Navbar and Services Section created

--- assets/js/observer.js
+++ assets/js/observer.js
@@ -1,12 +1,48 @@
-const observer = new IntersectionObserver((entries) => {
+navBar = document.querySelector('.nav');
+logoHeader = document.querySelector('.logo-header');
+navAnchor = document.querySelectorAll('.nav-anchor');
+ourServicesCard = document.querySelectorAll('.our-services-card');
+navLinks
+
+const pageHeaderObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('section-content-animation');
+            navBar.classList.remove('nav-white');
+            navAnchor.forEach((anchor) => {
+                anchor.classList.remove('nav-anchor-white');
+            });
+            logoHeader.classList.remove('logo-header-white');
         }else {
             entry.target.classList.remove('section-content-animation');
+            navBar.classList.add('nav-white');
+            navAnchor.forEach((anchor) => {
+                anchor.classList.add('nav-anchor-white');
+            });
+            logoHeader.classList.add('logo-header-white');
         }
     })
 });
 
-const elements = document.querySelectorAll('.section-content');
-elements.forEach((el) => observer.observe(el));
+const servicesObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('services-content-animation');
+            ourServicesCard.forEach((card) => {
+                card.classList.add('our-services-card-show');
+            });
+        }
+        else {
+            entry.target.classList.remove('services-content-animation');
+            ourServicesCard.forEach((card) => {
+                card.classList.remove('our-services-card-show');
+            });
+        }
+    })
+});
+
+const pageHeaderContent = document.querySelectorAll('.page-header-content');
+pageHeaderContent.forEach((el) => pageHeaderObserver.observe(el));
+
+const serviceContent = document.querySelectorAll('.our-services-content');
+serviceContent.forEach((el) => servicesObserver.observe(el));
---
+++ assets/js/scrollTracker.js
@@ -0,0 +1,45 @@
+const pageHeader = document.getElementById('page-header-content');
+const ourServices = document.getElementById('our-services-content');
+const about = document.getElementById('about-content');
+const contact = document.getElementById('contact-content');
+
+const homeNavLink = document.querySelector('.home-nav-link');
+const servicesNavLink = document.querySelector('.services-nav-link');
+const aboutNavLink = document.querySelector('.about-nav-link');
+const contactNavLink = document.querySelector('.contact-nav-link');
+
+const scrollTracker = () => {
+    const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    const pageHeaderOffset =  pageHeader.offsetTop + pageHeader.offsetHeight;
+    const ourServicesOffset =  ourServices.offsetTop + ourServices.offsetHeight;
+    console.log(scrollTop);
+    console.log(pageHeaderOffset);
+    // const aboutOffset =  about.offsetTop + about.offsetHeight;
+    // const contactOffset =  contact.offsetTop + contact.offsetHeight;
+
+    // if (scrollTop > aboutOffset) {
+    //     contactNavLink.classList.add('link-active');
+    //     homeNavLink.classList.remove('link-active');
+    //     servicesNavLink.classList.remove('link-active');
+    //     aboutNavLink.classList.remove('link-active');
+    // }
+    // if (scrollTop > ourServicesOffset) {
+    //     aboutNavLink.classList.add('link-active');
+    //     homeNavLink.classList.remove('link-active');
+    //     servicesNavLink.classList.remove('link-active');
+    //     contactNavLink.classList.remove('link-active');
+    // }
+    if (scrollTop > pageHeaderOffset) {
+        servicesNavLink.classList.add('link-active');
+        homeNavLink.classList.remove('link-active');
+        aboutNavLink.classList.remove('link-active');
+        contactNavLink.classList.remove('link-active');
+    } else {
+        homeNavLink.classList.add('link-active');
+        servicesNavLink.classList.remove('link-active');
+        aboutNavLink.classList.remove('link-active');
+        contactNavLink.classList.remove('link-active');
+    }
+}
+
+// window.onscroll = scrollTracker();
